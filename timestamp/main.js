8c593de6aa480c2f90d13d3f71ea555495cee455
JJ <jj.spetseris@yahoo.com>
2017-10-17 10:03:19 -0500

    unix works need natural

--- timestamp/main.js
+++ timestamp/main.js
@@ -5,20 +5,41 @@ app.use(express.static('public'));
 
 app.set('view engine', 'hbs');
 
+// month array
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+ 'August', 'September', 'October', 'November', 'December'];
+
 // routes
 app.get('/', function(request, response) {
   response.render('home.hbs');
 });
 
 app.get('/:slug', function(request, response) {
+  var slug = request.params.slug;
+  // console.log(slug);
+  var timestamp = new Date(parseInt(slug));
+  var valid = timestamp.getTime() >= 0;
+  // console.log(valid);
   var content = {
-    'unix timestamp': null,
-    'natural timestamp': null
+    'unix': null,
+    'natural': null
   }
   // if proper unix timestamp
+  if (valid) {
+    var timestring = months[timestamp.getUTCMonth()] + ' '
+                  +  timestamp.getUTCDate() + ', ' + timestamp.getUTCFullYear()
+    content = {
+      'unix': timestamp.getTime(),
+      'natural': timestring
+    }
+    response.send(content)
+  } else {
+    // else return null
+    response.send(content);
+  }
+  
   // if proper natural language timestamp
-  // else return null
-  response.send(content);
+
 });
 
 app.listen(8000, function() {
